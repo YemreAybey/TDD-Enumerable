c288c66ae5a183fc4dbeb6765f9b4f40e6c978b4
NeerNim <neernimt@gmail.com>
2019-05-31 17:15:19 +0200

    finish tests for all enumerable methods

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe Enumerable do
 
     end
 
-    describe "#my_select" do
+  describe "#my_select" do
     it "works on array" do
       array = [1, 2, 3, 4]
       expect(array.my_select{ |num| num > 2}).to eql([3, 4])
@@ -72,7 +72,60 @@ RSpec.describe Enumerable do
       hash = {a: "apple", b:"banana"}
       expect(hash.my_select{ |key, value| value.length > 5}).to eql({b: "banana"})
     end
+  end
+
+  describe "my_any? method" do
+    it "should return true if it find the true value in array" do
+      arr = [1, 2, 4]
+      expect(arr.my_any? { | num | num > 3}).to eql(true)
+   end 
+
+   it "should return true if it find the true value in hash" do
+      hash = {a: "apple", b:"banana"}
+      expect(hash.my_any? { | key, value | value.length > 3}).to eql(true)
+   end 
+
+   it "should return false if it does not finds true value" do
+      arr = [1, 2, 3]
+      expect(arr.my_any? { | num | num > 3}).to eql(false)
+   end 
+
+   it "should return false when not passed a block" do      
+      arr = [1, 2, 3]
+      expect(arr.my_any? { }).to eql(false)
+   end    
+  end
+ 
+  describe "my_all? method" do
+    it "should return false if it finds the false true value in array" do
+      arr = [1, 2, 4]
+      expect(arr.my_all? { | num | num > 3}).to eql(false)
+    end 
+    
+    it "should return false if it finds the false true value in hash" do
+      hash = {a: "apple", b:"banana"}
+      expect(hash.my_all? { | key, value | value.length > 3}).to eql(true)
+    end 
+
+    it "should return false if it finds nil value in array" do
+      arr = [nil, false, false]
+      expect(arr.my_all? { | num | num == false}).to eql(false)
+    end 
+
+  end  
+
+  describe "my_none? method should " do
+    it "should return false if there is one true situation in Array" do
+      arr = [1, 2, 3]
+      expect(arr.my_none? { | num | num > 2}).to eql(false)
+    end 
+
+    it "should return false if one true situation in hash" do
+      hash = {a: "apple", b:"banana"}
+      expect(hash.my_none? { | num, value | value.length > 2}).to eql(false)
+    end 
 
   end
+
 
 end
