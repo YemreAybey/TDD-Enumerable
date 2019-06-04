07739fc6a7fa50a0e023c4a88db56497b7acc675
NeerNim <neernimt@gmail.com>
2019-06-04 12:10:09 +0200

    code review change correction

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,15 +1,16 @@
 require './lib/enumarable'
 
-RSpec.describe Enumerable do
+describe Enumerable do
+    let(:array) { [1, 2, 3, 4] }
+    let(:hash) { {a: 1, b: 2} }
+    let(:hash1) { {a: "apple", b:"banana"} }
 
     describe "#my_each" do
       it "returns array" do
-        array = [1, 2, 3, 4]
-        expect(array.my_each{ |num| puts num}).to eql(array)
+        expect(array.my_each{ |num| num}).to eql(array)
       end
 
       it "returns hash" do
-        hash = {a: 1, b: 2}
         expect(hash.my_each{ |key, value|  key}).to eql(hash)
       end
 
@@ -21,32 +22,22 @@ RSpec.describe Enumerable do
 
     describe "#my_inject" do
       it "takes the first value when nothing is passed as parameter" do
-        array = [1, 2, 3, 4]
         expect(array.my_inject { |acc, value| acc + value}).to eql(10)
       end
 
       it "takes a parameter" do
-        array = [1, 2, 3, 4]
         expect(array.my_inject(1) { |acc, value| acc + value}).to eql(11)
-      end
-
-      it "takes a string as parameter returns the string multiply by number" do
-        array = [1, 1, 1, 2]
-        expect(array.my_inject("neer") { |acc, value| acc * value}).to eql("neerneer")
       end
         
       it "works on hash" do
-        hash = {a: 1, b: 2}
         expect(hash.my_inject { |acc, key, value| acc * value}).to eql(2)
       end
 
       it "works on hash when passed parameter" do
-        hash = {a: 1, b: 2}
         expect(hash.my_inject(2.5) { |acc, key, value| acc * value}).to eql(5.0)
       end
 
       it "works on hash when passed string parameter" do
-        hash = {a: 1, b: 2}
         expect(hash.my_inject("yem") { |acc, key, value| acc * value}).to eql("yemyem")
       end
 
@@ -54,12 +45,10 @@ RSpec.describe Enumerable do
 
   describe "#my_select" do
     it "works on array" do
-      array = [1, 2, 3, 4]
       expect(array.my_select{ |num| num > 2}).to eql([3, 4])
     end
 
     it "works on hash" do
-      hash = {a: 1, b: 2}
       expect(hash.my_select{ |key, value| value > 2}).to eql({})
     end
 
@@ -69,63 +58,53 @@ RSpec.describe Enumerable do
     end
 
     it "works on string values" do
-      hash = {a: "apple", b:"banana"}
-      expect(hash.my_select{ |key, value| value.length > 5}).to eql({b: "banana"})
+      expect(hash1.my_select{ |key, value| value.length > 5}).to eql({b: "banana"})
     end
   end
 
   describe "my_any? method" do
     it "should return true if it find the true value in array" do
-      arr = [1, 2, 4]
-      expect(arr.my_any? { | num | num > 3}).to eql(true)
+      expect(array.my_any? { | num | num > 3}).to eql(true)
    end 
 
    it "should return true if it find the true value in hash" do
-      hash = {a: "apple", b:"banana"}
-      expect(hash.my_any? { | key, value | value.length > 3}).to eql(true)
+      expect(hash1.my_any? { | key, value | value.length > 3}).to eql(true)
    end 
 
    it "should return false if it does not finds true value" do
-      arr = [1, 2, 3]
-      expect(arr.my_any? { | num | num > 3}).to eql(false)
+      expect(array.my_any? { | num | num > 4}).to eql(false)
    end 
 
    it "should return false when not passed a block" do      
-      arr = [1, 2, 3]
-      expect(arr.my_any? { }).to eql(false)
+      expect(array.my_any? { }).to eql(false)
    end    
   end
  
   describe "my_all? method" do
     it "should return false if it finds the false true value in array" do
-      arr = [1, 2, 4]
-      expect(arr.my_all? { | num | num > 3}).to eql(false)
+      expect(array.my_all? { | num | num > 3}).to eql(false)
     end 
     
     it "should return false if it finds the false true value in hash" do
-      hash = {a: "apple", b:"banana"}
-      expect(hash.my_all? { | key, value | value.length > 3}).to eql(true)
+      expect(hash1.my_all? { | key, value | value.length > 3}).to eql(true)
     end 
 
     it "should return false if it finds nil value in array" do
       arr = [nil, false, false]
-      expect(arr.my_all? { | num | num == false}).to eql(false)
+      expect(array.my_all? { | num | num == false}).to eql(false)
     end 
 
   end  
 
   describe "my_none? method should " do
     it "should return false if there is one true situation in Array" do
-      arr = [1, 2, 3]
-      expect(arr.my_none? { | num | num > 2}).to eql(false)
+      expect(array.my_none? { | num | num > 2}).to eql(false)
     end 
 
     it "should return false if one true situation in hash" do
-      hash = {a: "apple", b:"banana"}
-      expect(hash.my_none? { | num, value | value.length > 2}).to eql(false)
+      expect(hash1.my_none? { | num, value | value.length > 2}).to eql(false)
     end 
 
   end
-
 
 end
